cc51d692178b558a1e7ea97a256d85c8ab39913c
Eswarvallabha <eashwarvallabha180@gmail.com>
2024-12-23 16:23:11 +0530

    Update Projects

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -6,13 +6,15 @@ export const ContactMe = () => {
             <div className='text-[40px] font-bold text-center font-serif'>Contact Me</div>
             <div className='md:grid md:grid-cols-2 md:m-3 w-screen'>
                 <div className='md:flex md:flex-col md:gap-3 items-center'>
-                    <label className='text-white' htmlFor="">Enter Email: </label>
-                    <input type="email" className='p-2 m-4 w-fit' placeholder='Enter Email...'/>
+                    <label className='text-white md:text-start md:w-full' htmlFor="">Enter Email: </label>
+                    <input type="email" className='p-2 m-4 w-fit rounded-xl md:w-full' placeholder='Enter Email...'/>
                     <br />
-                    <label className='text-white' htmlFor="">Enter Password: </label>
-                    <input type="password" className='p-2 m-2 w-fit' placeholder='Enter Password...' />
+                    <label className='text-white md:text-start md:w-full' htmlFor="">Enter Password: </label>
+                    <input type="password" className='p-2 m-4 w-fit rounded-xl md:w-full' placeholder='Enter Password...' />
                     <br />
-                    <textarea name="" className='h-40 p-2 m-2 w-fit' placeholder='Enter the message you want to send........' id=""></textarea>
+                    <label className='text-white md:text-start md:w-full' htmlFor="">Message </label>
+
+                    <textarea name="" className='p-2 m-4 w-fit rounded-xl md:w-full' placeholder='Enter the message you want to send........' id=""></textarea>
                     <div className='flex justify-center w-full'>
                         <button className='bg-blue-700 p-3 rounded-xl'>Submit</button>
                     </div>
